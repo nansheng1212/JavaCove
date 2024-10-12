35797d3d09d4db9f8d53657580055b1b2ebfac2a
郭俊 <guo.jun26@iwhalecloud.com>
2024-10-12 09:36:02 +0800

    移除前端配置ip

--- front/shoka-admin/vite.config.ts
+++ front/shoka-admin/vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: "http://123.57.1.39:8080",
+        target: "http://localhost:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
